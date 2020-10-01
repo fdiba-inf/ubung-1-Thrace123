a86630223745973d781a9c76be2e8e991c3f4a96
Thrace123 <72183930+Thrace123@users.noreply.github.com>
2020-10-01 06:06:36 +0000

    11

--- src/exercise1/FdibaOutput.java
+++ src/exercise1/FdibaOutput.java
@@ -3,11 +3,11 @@ package exercise1;
 public class FdibaOutput {
 
   public static void main(String[] args) {
-    System.out.println(" FFFF   DDDD    II  BBBB          A");
-    System.out.println(" F      D   D   II  B   B        A  A");
-    System.out.println(" FFFF   D     D II  BBBB        AAAAAA");
-    System.out.println(" F      D   D   II  B   B       A     A");
-    System.out.println(" F      DDDD    II  BBBB       A        A");
+    System.out.println(" FFFF   DDDD    II  BBBB      A");
+    System.out.println(" F      D   D   II  B   B    A A");
+    System.out.println(" FFFF   D     D II  BBBB    AAAAA");
+    System.out.println(" F      D   D   II  B   B  A     A");
+    System.out.println(" F      DDDD    II  BBBB  A       A");
 
 
   }
